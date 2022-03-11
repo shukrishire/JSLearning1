36cc2cfedafa9d5e933494e63c6b525f5882126b
shukrishire <shukrishire28@gmail.com>
2022-03-12 05:46:23 +1300

    Functions in JS bBasics

---
+++ js3.js
@@ -0,0 +1,8 @@
+//sampple function 
+function Addition(){
+    //whatever is written here is part of function
+    alert("Hello I am a function")
+}
+// js would now run this function
+
+Addition(); 
